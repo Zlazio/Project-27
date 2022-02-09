3cbcbe08dc48b64cfd45cfce99f640bb25b0cdaa
Zlazio <95372448+Zlazio@users.noreply.github.com>
2022-02-09 16:33:15 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine, world;
+var ball, ground, wedge, poly;
+var angle = 60;
+var boxes = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var option = {
+    isStatic: true,
+  }
+
+  ground = Bodies.rectangle(100, 300, 400, 20, option);
+  World.add(world, ground);
+  box1 = new Box(200, 100, 10, 10);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function mousePressed() {
+  boxes.push(
+    new Box(mouseX, mouseY, 50, 50)
+    )
+}
+
+function draw() {
+  background(51);
+  rect(ground.position.x, ground.position.y, 400, 10);
+  Engine.update(engine);
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+}
